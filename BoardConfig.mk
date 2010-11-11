8524aa5f7e9cf9aec5bd9d99cf831ee5e759d3bc
Koushik Dutta <koushd@gmail.com>
2010-11-11 00:07:00 -0800

    Remove reference to updater lib.
    Change-Id: I05bfb78c808e011add66df0827f0077b26d2e4a8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_RECOVERY_UI_LIB := librecovery_ui_bravoc librecovery_ui_htc
 
-TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_htc
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 TARGET_PREBUILT_KERNEL := device/htc/bravoc/kernel/kernel
