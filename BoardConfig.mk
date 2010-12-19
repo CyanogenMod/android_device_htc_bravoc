1378f14b3a423d6aeff935b1effc6303e2f04562
Giulio Cervera <giulio.cervera@gmail.com>
2010-12-19 22:12:39 +0100

    bravoc: update gps var

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,10 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_RECOVERY_UI_LIB := librecovery_ui_bravoc librecovery_ui_htc
 
-
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 TARGET_PREBUILT_KERNEL := device/htc/bravoc/kernel/kernel
+
+# to enable the GPS HAL
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := bravoc
+# AMSS version to use for GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 3200
