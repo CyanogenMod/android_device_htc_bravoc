71e391097e6c61592980debd75e253e5e4fdb5e3
Giulio Cervera <giulio.cervera@gmail.com>
2010-12-20 22:41:33 +0100

    bravoc: update extract-files.sh setup-makefiles.sh unzip-files.sh based on Passion G

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_bravoc-userdebug
-add_lunch_combo full_bravoc-eng
+add_lunch_combo full_bravo-userdebug
+add_lunch_combo full_bravo-eng
