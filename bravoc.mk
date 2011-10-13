2ba3eefabbdd6699f8f59eafa9f1149b4683c475
Giulio Cervera <giulio.cervera@gmail.com>
2011-10-13 22:50:32 +0200

    bravoc: overide dirty_ratio on UMS
    Change-Id: I2d19b751bdd08212bfca35897454140086ae8d1f

--- bravoc.mk
+++ bravoc.mk
@@ -63,6 +63,10 @@ $(call inherit-product-if-exists, vendor/htc/bravoc/bravoc-vendor.mk)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.media.dec.jpeg.memcap=20000000
 
+# Don't set /proc/sys/vm/dirty_ratio to 0 when USB mounting
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vold.umsdirtyratio=20
+
 DEVICE_PACKAGE_OVERLAYS += device/htc/bravoc/overlay
 
 PRODUCT_COPY_FILES += \
