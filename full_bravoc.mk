c1d2622e0a742759e6266802f04cef2e8560d964
Steve Kondik <shade@chemlab.org>
2011-10-28 11:01:14 -0700

    bravoc: Fix PRODUCT_LOCALES
    Change-Id: I8b2a9deae9ea4b2910af5286802cf4867a5fc186

--- full_bravoc.mk
+++ full_bravoc.mk
@@ -23,6 +23,9 @@
 $(call inherit-product, device/htc/bravoc/bravoc_us.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
+# Bravo uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_bravoc
 PRODUCT_DEVICE := bravoc
